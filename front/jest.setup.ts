0f5c93570339a64d992b570889c4b55b82de4282
Ivan Fadeev <ifadeev@lanit.ru>
2025-06-08 15:00:12 +0300

    deploy: сделана сборка для proxy

--- front/jest.setup.ts
+++
@@ -1,5 +0,0 @@
-import { TextEncoder, TextDecoder } from 'util';
-import 'whatwg-fetch';
-
-(global as any).TextEncoder = TextEncoder;
-(global as any).TextDecoder = TextDecoder;
--- front/src/features/rates/types.ts
+++
@@ -1,4 +0,0 @@
-export type Rate = {
-  code: string;
-  rate: string;
-};
