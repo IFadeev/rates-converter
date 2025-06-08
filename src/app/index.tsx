57067fbde9d18458e5b6ffec801deb1b1663e464
Ivan Fadeev <ifadeev@lanit.ru>
2025-06-08 17:03:25 +0300

    deplay: фикс статуса 404

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './store';
 import AppRoutes from './routes';
+import { HashRouter as Router } from 'react-router-dom';
 import '@/shared/styles/globals.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
@@ -11,7 +12,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <AppRoutes />
+        <Router>
+          <AppRoutes />
+        </Router>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
